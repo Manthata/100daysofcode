ceb52dbfe808e28bfda7fb0609c5b3be3f5b34f8
Manthata William <32458823+Manthata@users.noreply.github.com>
2023-04-21 20:40:27 +0900

    refactor day 9

---
+++ day9/main.py
@@ -0,0 +1,41 @@
+from typing import Union
+
+from fastapi import FastAPI, Response
+import qrcode
+from io import BytesIO
+
+app = FastAPI()
+
+
+@app.get("/generate/QR")
+def generate(url: str) -> bytes:
+    """
+    This function is responsible for generating qr code for the url inputed
+
+    Args:
+        url (str): url you want generate a qr code for
+
+    Returns:
+        bytes: image in a form of bytes
+    """
+
+    qr = qrcode.QRCode(version=1,
+                       box_size=10,
+                       border=5)
+
+    # Adding data to the instance 'qr'
+    qr.add_data(url)
+
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black',
+                        back_color='white')
+
+    # Convert the PIL image object to bytes
+    img_bytes = BytesIO()
+    img.save(img_bytes)
+    img_bytes = img_bytes.getvalue()
+
+    # Create a FastAPI response object with the image bytes and content type
+    response = Response(content=img_bytes, media_type="image/png")
+
+    return response
---
+++ day9/readme.md
@@ -0,0 +1,17 @@
+# 100daysofcode
+Update for Day 9 date 15 JAN 2023 
+## Quote for today/message: 
+I've always wanted to create a few endpoints that I can use in my own personal life,
+and at work. This time I created a QR code generator endpoint. Hopefully in the future 
+I will deploy it to some cloud environment and be able to create my own QR codes without 
+going through the websites full of adverts. 
+## Objective: 
+
+QR code genrator on FastAPI
+
+## Lessons
+Nothing much 
+## Reflections
+Makefile are pretty usefull when you don't want to remember to command to start up your server
+## Conclusion 
+Great refresher
